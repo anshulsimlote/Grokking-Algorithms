5299fda1d9e8195fec3b9faa2c0c05060bde422d
anshulsimlote <anshulsimlote1999@gmail.com>
2025-09-09 18:39:02 +0530

    Improved code

--- quick_sort.py
+++ quick_sort.py
@@ -1,32 +1,15 @@
-data = [2,4,1,6,9,7,5,8]
+# Original data
+data = [2, 4, 1, 6, 9, 7, 5, 8]
 
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr  
+    pivot = arr[0]
+    left = [x for x in arr[1:] if x < pivot]
+    right = [x for x in arr[1:] if x >= pivot]
+    return quick_sort(left) + [pivot] + quick_sort(right)
 
-def  quickSort(data):
-    res = []
-    if(len(data) <= 1):
-        return data
-    if(len(data) == 2):
-        if data[0] > data[1]:
-            res.append(data[1])
-            res.append(data[0])
-        else:
-            res.append(data[0])
-            res.append(data[1])
-        return res
-    pivot = data[0]
-    l = []
-    r = []
-    for index in range(1,len(data)):
-        val = data[index]
-        if(val < pivot):
-            l.append(val)
-        else:
-            r.append(val)
-    res.extend(quickSort(l))
-    res.append(pivot)
-    res.extend(quickSort(r))
-    return res
+sorted_data = quick_sort(data)
 
-print(f"data : {data}")
-
-print(f"Sorted : {quickSort(data)}")
+print(f"Original data: {data}") 
+print(f"Sorted data: {quick_sort(data)}")
