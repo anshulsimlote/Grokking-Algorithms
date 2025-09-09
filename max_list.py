3d07ccd2756ac531e5d56c4498ea48cf83efb09e
anshulsimlote <anshulsimlote1999@gmail.com>
2025-09-09 15:20:09 +0530

    Max with Divide and concur with slicing

--- max_list.py
+++ max_list.py
@@ -1,11 +1,12 @@
 data = [1, 2, 8, 2, 9, 0]
 
-def max_item(items, index=0):
-    """Recursively find the maximum element in a list (no slicing)."""
-    if index == len(items) - 1:   # base case: last element
-        return items[index]
-    
-    rest_max = max_item(items, index + 1)
-    return items[index] if items[index] > rest_max else rest_max
+def max_item(items):
+   if( len(items) == 1):
+      return items[0]
+   mid = len(items)//2
+   left_max = max_item(items[:mid])
+   right_max = max_item(items[mid:])
+   return left_max if left_max > right_max else right_max
+
 
 print(f"Max: {max_item(data)}")
